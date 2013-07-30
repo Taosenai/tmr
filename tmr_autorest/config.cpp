439005a55fcfb420a6fe2b5ea5a006c936c2db51
Taosenai <schultz.ryan@gmail.com>
2013-07-30 01:53:45 -0400

    Add automatic resting and manual bipod deployment module (autorest)

---
+++ tmr_autorest/config.cpp
@@ -0,0 +1,456 @@
+#define true	1
+#define false	0
+
+class CfgPatches {
+	class tmr_autorest {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.60;
+		requiredAddons[] = {A3_Weapons_F, "A3_Anims_F", tmr_core};
+		version = 0.1;
+		author[] = {"Taosenai"};
+		authorUrl = "http://www.ryanschultz.org/tmr/";
+	};
+};
+
+class CfgMods {
+	class tmr_autorest {
+		dir = "tmr_autorest";
+		name = "TMR: Weapon Autorest Module";
+		picture = "";
+		hidePicture = "true";
+		hideName = "true";
+		actionName = "Website";
+		action = "http://www.ryanschultz.org/tmr/";
+	};
+};
+
+class Extended_PostInit_EventHandlers {
+	class tmr_autorest {
+		clientInit = "call compile preProcessFileLineNumbers '\tmr_autorest\init.sqf'";
+	};
+};
+
+class CfgVehicles {
+	class Helper_Base_F;
+	class TMR_Autorest_GeoRef: Helper_Base_F {
+		accuracy = 1000;
+		displayname = "";
+		icon = "";
+		model = "\A3\Weapons_F\empty.p3d";
+		scope = 2;
+	};
+};
+
+#define TMR_SWAY_DEPLOY 0.094
+#define TMR_DEPLOY_TURNSPEED 0.0001
+
+// Arma 3 doesn't respect turnSpeed.
+
+class CfgMovesBasic {
+	class Default;
+
+	class Actions {
+		class RifleStandActions;
+		class RifleStandActions_tmr_deploy : RifleStandActions {
+			stop =    "AmovPercMstpSrasWrflDnon_tmr_deploy";
+			default = "AmovPercMstpSrasWrflDnon_tmr_deploy";
+			turnL = "AmovPercMstpSrasWrflDnon_tmr_deploy";
+			turnR = "AmovPercMstpSrasWrflDnon_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleAdjustLStandActions;
+		class RifleAdjustLStandActions_tmr_deploy : RifleAdjustLStandActions {
+			stop =    "AadjPercMstpSrasWrflDleft_tmr_deploy";
+			default = "AadjPercMstpSrasWrflDleft_tmr_deploy";
+			AdjustL = "AadjPercMstpSrasWrflDleft_tmr_deploy";
+			turnL = "AadjPercMstpSrasWrflDleft_tmr_deploy";
+			turnR = "AadjPercMstpSrasWrflDleft_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleAdjustRStandActions;
+		class RifleAdjustRStandActions_tmr_deploy : RifleAdjustRStandActions {
+			stop =    "AadjPercMstpSrasWrflDright_tmr_deploy";
+			default = "AadjPercMstpSrasWrflDright_tmr_deploy";
+			AdjustRight = "AadjPercMstpSrasWrflDright_tmr_deploy";
+			turnL = "AadjPercMstpSrasWrflDright_tmr_deploy";
+			turnR = "AadjPercMstpSrasWrflDright_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleAdjustFStandActions;
+		class RifleAdjustFStandActions_tmr_deploy : RifleAdjustFStandActions {
+			stop =    "AadjPercMstpSrasWrflDup_tmr_deploy";
+			default = "AadjPercMstpSrasWrflDup_tmr_deploy";
+			AdjustF = "AadjPercMstpSrasWrflDup_tmr_deploy";
+			turnL = "AadjPercMstpSrasWrflDup_tmr_deploy";
+			turnR = "AadjPercMstpSrasWrflDup_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleAdjustBStandActions;
+		class RifleAdjustBStandActions_tmr_deploy : RifleAdjustBStandActions {
+			stop =    "AadjPercMstpSrasWrflDdown_tmr_deploy";
+			default = "AadjPercMstpSrasWrflDdown_tmr_deploy";
+			AdjustB = "AadjPercMstpSrasWrflDdown_tmr_deploy";
+			turnR = "AadjPercMstpSrasWrflDdown_tmr_deploy";
+			turnL = "AadjPercMstpSrasWrflDdown_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleAdjustLKneelActions;
+		class RifleAdjustLKneelActions_tmr_deploy : RifleAdjustLKneelActions {
+			stop =    "AadjPknlMstpSrasWrflDleft_tmr_deploy";
+			default = "AadjPknlMstpSrasWrflDleft_tmr_deploy";
+			turnL = "AadjPknlMstpSrasWrflDleft_tmr_deploy";
+			turnR = "AadjPknlMstpSrasWrflDleft_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleAdjustRKneelActions;
+		class RifleAdjustRKneelActions_tmr_deploy : RifleAdjustRKneelActions {
+			stop =    "AadjPknlMstpSrasWrflDright_tmr_deploy";
+			default = "AadjPknlMstpSrasWrflDright_tmr_deploy";
+			turnL = "AadjPknlMstpSrasWrflDright_tmr_deploy";
+			turnR = "AadjPknlMstpSrasWrflDright_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleAdjustFKneelActions;
+		class RifleAdjustFKneelActions_tmr_deploy : RifleAdjustFKneelActions {
+			stop =    "AadjPknlMstpSrasWrflDup_tmr_deploy";
+			default = "AadjPknlMstpSrasWrflDup_tmr_deploy";
+			turnL = "AadjPknlMstpSrasWrflDup_tmr_deploy";
+			turnR = "AadjPknlMstpSrasWrflDup_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleAdjustBKneelActions;
+		class RifleAdjustBKneelActions_tmr_deploy : RifleAdjustBKneelActions {
+			stop =    "AadjPknlMstpSrasWrflDdown_tmr_deploy";
+			default = "AadjPknlMstpSrasWrflDdown_tmr_deploy";
+			turnL = "AadjPknlMstpSrasWrflDdown_tmr_deploy";
+			turnR = "AadjPknlMstpSrasWrflDdown_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleKneelActions;
+		class RifleKneelActions_tmr_deploy : RifleKneelActions {
+			stop = "AmovPknlMstpSrasWrflDnon_tmr_deploy";
+			default = "AmovPknlMstpSrasWrflDnon_tmr_deploy";
+			crouch = "AmovPknlMstpSrasWrflDnon_tmr_deploy";
+			turnL = "AmovPknlMstpSrasWrflDnon_tmr_deploy";
+			turnR = "AmovPknlMstpSrasWrflDnon_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleProneActions;
+		class RifleProneActions_tmr_deploy : RifleProneActions {
+			stop = "AmovPpneMstpSrasWrflDnon_tmr_deploy";
+			default = "AmovPpneMstpSrasWrflDnon_tmr_deploy";
+			turnL = "AmovPpneMstpSrasWrflDnon_tmr_deploy";
+			turnR = "AmovPpneMstpSrasWrflDnon_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+
+		class RifleAdjustFProneActions;
+		class RifleAdjustFProneActions_tmr_deploy : RifleAdjustFProneActions {
+			stop = "aadjppnemstpsraswrfldup_tmr_deploy";
+			default = "aadjppnemstpsraswrfldup_tmr_deploy";
+			turnL = "aadjppnemstpsraswrfldup_tmr_deploy";
+			turnR = "aadjppnemstpsraswrfldup_tmr_deploy";
+			turnSpeed = TMR_DEPLOY_TURNSPEED;
+			limitFast = 1;
+		};
+	};
+};
+
+class CfgMovesMaleSdr : CfgMovesBasic {	
+	class States {
+		class AmovPercMstpSrasWrflDnon;
+		class AmovPercMstpSrasWrflDnon_tmr_deploy : AmovPercMstpSrasWrflDnon {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleStandActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"AmovPercMstpSrasWrflDnon_tmr_deploy", 0.02};
+			ConnectFrom[] = {"AmovPercMstpSrasWrflDnon_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"AmovPercMstpSrasWrflDnon", 0.02};
+			InterpolateTo[] = {"AmovPercMstpSrasWrflDnon", 0.02};
+		};
+
+		class aadjpercmstpsraswrfldup;
+		class aadjpercmstpsraswrfldup_tmr_deploy : aadjpercmstpsraswrfldup {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleAdjustFStandActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"aadjpercmstpsraswrfldup_tmr_deploy", 0.02};
+			ConnectFrom[] = {"aadjpercmstpsraswrfldup_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"aadjpercmstpsraswrfldup", 0.02};
+			InterpolateTo[] = {"aadjpercmstpsraswrfldup", 0.02};
+		};
+
+		class aadjpercmstpsraswrflddown;
+		class aadjpercmstpsraswrflddown_tmr_deploy : aadjpercmstpsraswrflddown {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleAdjustBStandActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"aadjpercmstpsraswrflddown_tmr_deploy", 0.02};
+			ConnectFrom[] = {"aadjpercmstpsraswrflddown_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"aadjpercmstpsraswrflddown", 0.02};
+			InterpolateTo[] = {"aadjpercmstpsraswrflddown", 0.02};
+		};
+
+		class aadjpercmstpsraswrfldright;
+		class aadjpercmstpsraswrfldright_tmr_deploy : aadjpercmstpsraswrfldright {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleAdjustRStandActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"aadjpercmstpsraswrfldright_tmr_deploy", 0.02};
+			ConnectFrom[] = {"aadjpercmstpsraswrfldright_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"aadjpercmstpsraswrfldright", 0.02};
+			InterpolateTo[] = {"aadjpercmstpsraswrfldright", 0.02};
+		};
+
+		class aadjpercmstpsraswrfldleft;
+		class aadjpercmstpsraswrfldleft_tmr_deploy : aadjpercmstpsraswrfldleft {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleAdjustLStandActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"aadjpercmstpsraswrfldleft_tmr_deploy", 0.02};
+			ConnectFrom[] = {"aadjpercmstpsraswrfldleft_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"aadjpercmstpsraswrfldleft", 0.02};
+			InterpolateTo[] = {"aadjpercmstpsraswrfldleft", 0.02};
+		};
+
+		class aadjpknlmstpsraswrfldup;
+		class aadjpknlmstpsraswrfldup_tmr_deploy : aadjpknlmstpsraswrfldup {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleAdjustFKneelActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"aadjpknlmstpsraswrfldup_tmr_deploy", 0.02};
+			ConnectFrom[] = {"aadjpknlmstpsraswrfldup_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"aadjpknlmstpsraswrfldup", 0.02};
+			InterpolateTo[] = {"aadjpknlmstpsraswrfldup", 0.02};
+		};
+
+		class amovpknlmstpsraswrfldnon;
+		class amovpknlmstpsraswrfldnon_tmr_deploy : amovpknlmstpsraswrfldnon {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleKneelActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"amovpknlmstpsraswrfldnon_tmr_deploy", 0.02};
+			ConnectFrom[] = {"amovpknlmstpsraswrfldnon_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"amovpknlmstpsraswrfldnon", 0.02};
+			InterpolateTo[] = {"amovpknlmstpsraswrfldnon", 0.02};
+		};
+
+		class aadjpknlmstpsraswrflddown;
+		class aadjpknlmstpsraswrflddown_tmr_deploy : aadjpknlmstpsraswrflddown {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleAdjustBKneelActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"aadjpknlmstpsraswrflddown_tmr_deploy", 0.02};
+			ConnectFrom[] = {"aadjpknlmstpsraswrflddown_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"aaadjpknlmstpsraswrflddown", 0.02};
+			InterpolateTo[] = {"aadjpknlmstpsraswrflddown", 0.02};
+		};
+
+		class aadjpknlmstpsraswrfldleft;
+		class aadjpknlmstpsraswrfldleft_tmr_deploy : aadjpknlmstpsraswrfldleft {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleAdjustLKneelActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"aadjpknlmstpsraswrfldleft_tmr_deploy", 0.02};
+			ConnectFrom[] = {"aadjpknlmstpsraswrfldleft_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"aadjpknlmstpsraswrfldleft", 0.02};
+			InterpolateTo[] = {"aadjpknlmstpsraswrfldleft", 0.02};
+		};
+
+		class aadjpknlmstpsraswrfldright;
+		class aadjpknlmstpsraswrfldright_tmr_deploy : aadjpknlmstpsraswrfldright {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleAdjustRKneelActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"aadjpknlmstpsraswrfldright_tmr_deploy", 0.02};
+			ConnectFrom[] = {"aadjpknlmstpsraswrfldright_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"aadjpknlmstpsraswrfldright", 0.02};
+			InterpolateTo[] = {"aadjpknlmstpsraswrfldright", 0.02};
+		};
+
+		class aadjppnemstpsraswrfldup;
+		class aadjppnemstpsraswrfldup_tmr_deploy : aadjppnemstpsraswrfldup {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleAdjustFProneActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"aadjppnemstpsraswrfldup_tmr_deploy", 0.02};
+			ConnectFrom[] = {"aadjppnemstpsraswrfldup_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"aadjppnemstpsraswrfldup", 0.02};
+			InterpolateTo[] = {"aadjppnemstpsraswrfldup", 0.02};
+		};
+
+		class amovppnemstpsraswrfldnon;
+		class amovppnemstpsraswrfldnon_tmr_deploy : amovppnemstpsraswrfldnon {
+			aimPrecision = TMR_SWAY_DEPLOY;
+			actions = "RifleProneActions_tmr_deploy";
+			aiming = "aimingLying";
+			speed = 0.01;
+			onLandEnd = true;
+			onLandBeg = true;
+
+			ConnectTo[] = {"amovppnemstpsraswrfldnon_tmr_deploy", 0.02};
+			ConnectFrom[] = {"amovppnemstpsraswrfldnon_tmr_deploy", 0.02};
+			InterpolateFrom[] = {"amovppnemstpsraswrfldnon", 0.02};
+			InterpolateTo[] = {"amovppnemstpsraswrfldnon", 0.02};
+		};
+	};
+};
+
+class CfgWeapons {
+	class Rifle_Base_F;
+	class arifle_MX_Base_F;
+
+	class arifle_MX_SW_F : arifle_MX_Base_F {
+		tmr_autorest_deployable = 1;
+	};
+
+	class arifle_MXM_F : arifle_MX_Base_F {
+		tmr_autorest_deployable = 1;
+	};
+
+	////////////////////////////////////////////////
+
+	class Rifle_Long_Base_F;
+
+	class LMG_Mk200_F : Rifle_Long_Base_F {
+		tmr_autorest_deployable = 1;
+	};
+
+	class LMG_Zafir_F: Rifle_Long_Base_F {
+		tmr_autorest_deployable = 1;
+	};
+
+	class EBR_base_F : Rifle_Long_Base_F {
+		tmr_autorest_deployable = 1;
+	};
+
+	class LRR_base_F : Rifle_Long_Base_F {
+		tmr_autorest_deployable = 1;
+	};
+
+	class GM6_base_F : Rifle_Long_Base_F {
+		tmr_autorest_deployable = 1;
+	};
+};
+
+class TMR_RscPicture
+{
+   type = 0;
+   style = 48;
+   idc = -1;
+   colorBackground[] = {1, 1, 1, 1};
+   colorText[] = {1, 1, 1, 1};
+   font = "PuristaMedium";
+   size = 0;
+   sizeEx = 1;
+   lineSpacing = 1.0; 
+};
+
+class RscTitles {
+	class TMR_Autorest_Rested {
+		idd = -1;
+		onLoad = "with uiNameSpace do { TMR_Autorest_Rested = _this select 0 };";
+		onUnload = "";
+		movingEnable = 1;
+		duration = 10000;
+		controls[] = {"TMR_Autorest_Icon"};
+
+		class TMR_Autorest_Icon : TMR_RscPicture {
+			idc = 1;
+			movingEnable = 1;
+			colorBackground[] = {0, 0, 0, 0};
+			text = "\tmr_autorest\data\restedWeapon_ca.paa";
+			colorText[] = {1,1,1,1};
+			x = (SafeZoneW + SafeZoneX) - (1 - 0.1100);
+			y = (SafeZoneY + SafeZoneH) - (1 - 0.6474);
+			w = 0.061;
+			h = 0.061 / (getResolution select 5);
+		};
+	};
+	class TMR_Autorest_Deployed {
+		idd = -1;
+		onLoad = "with uiNameSpace do { TMR_Autorest_Deployed = _this select 0 };";
+		onUnload = "";
+		movingEnable = 1;
+		duration = 10000;
+		controls[] = {"TMR_Autorest_DeployedIcon"};
+
+		class TMR_Autorest_DeployedIcon : TMR_RscPicture {
+			idc = 1;
+			movingEnable = 1;
+			colorBackground[] = {0, 0, 0, 0};
+			text = "\tmr_autorest\data\deployWeapon_ca.paa";
+			colorText[] = {1,1,1,1};
+			x = (SafeZoneW + SafeZoneX) - (1 - 0.1100);
+			y = (SafeZoneY + SafeZoneH) - (1 - 0.6474);
+			w = 0.061;
+			h = 0.061 / (getResolution select 5);
+		};
+	};
+};
---
+++ tmr_autorest/init.sqf
@@ -0,0 +1,300 @@
+// TMR: Autorest initialization and functions
+// (C) 2013 Ryan Schultz. See LICENSE.
+
+tmr_autorest = false;
+
+// Set global variables
+tmr_autorest_restIconDisplayed = false;
+tmr_autorest_restIconTransition = false;
+tmr_autorest_deployIconDisplayed = false;
+tmr_autorest_deployIconTransition = false;
+
+#define TMR_AUTOREST_RESTEDRECOIL 0.61
+#define TMR_AUTOREST_DEPLOYEDRECOIL 0.495
+
+// -------------------------------------------------------------------------------
+// Key EH: Deploy the weapon if it has a bipod and there is something to deploy on.
+// -------------------------------------------------------------------------------
+tmr_autorest_fnc_deployKeyDownEH = {
+	// _displayCtrl = _this select 0;
+	_dikCode = _this select 1;
+	_shift = _this select 2;
+	_ctrl = _this select 3;
+	_alt = _this select 4;
+
+	_return = false;
+
+	// Key press to deploy
+	if (_dikCode in actionKeys "LockTargets") then {
+		// Player must be on foot, with primary out, able to fire.
+		if !(speed player < 1 && vehicle player == player && currentWeapon player == primaryWeapon player && canFire player && canMove player) exitWith {false};
+
+		// Make sure weapon is deployable (bipod mounted)
+		_config = configFile >> "CfgWeapons" >> currentWeapon player;
+		_canDeploy = getNumber (_config >> "tmr_autorest_deployable"); // 1 for true
+		if (_canDeploy != 1) exitwith {false};
+
+		// Make sure it's an anim that can deploy.
+		_allowedAnimStates = ["amovpercmstpsraswrfldnon", "aadjpercmstpsraswrfldup", "aadjpercmstpsraswrflddown", "aadjpercmstpsraswrfldright", "aadjpercmstpsraswrfldleft", "aadjpknlmstpsraswrfldup", "amovpknlmstpsraswrfldnon", "aadjpknlmstpsraswrflddown", "aadjpknlmstpsraswrfldleft", "aadjpknlmstpsraswrfldright", "aadjppnemstpsraswrfldup", "amovppnemstpsraswrfldnon"];
+		_playerAnimState = animationState player;
+		if !(_playerAnimState in _allowedAnimStates) exitWith {false};
+
+		// Check if the bipod area intersects with something or the ground.
+		_frontCheck = lineIntersectsWith [getposASL tmr_autorest_refPoint7, getposASL tmr_autorest_refPoint8, tmr_autorest_refPoint7, tmr_autorest_refPoint8];
+		_rearCheck = lineIntersectsWith [getposASL tmr_autorest_refPoint8, getposASL tmr_autorest_refPoint9, tmr_autorest_refPoint8, tmr_autorest_refPoint9];
+		_bottomCheck = lineIntersectsWith [getposASL tmr_autorest_refPoint7, getposASL tmr_autorest_refPoint9, tmr_autorest_refPoint7, tmr_autorest_refPoint9];
+		_terrainCheck  = terrainIntersectASL [getposASL tmr_autorest_refPoint7, getposASL tmr_autorest_refPoint9];
+
+		if (count _frontCheck > 0 || count _rearCheck > 0 || count _bottomCheck > 0 || _terrainCheck) then {
+			_return = true;
+
+			// End rest state before deploying.
+			if (player getVariable ["tmr_autorest_rested", false]) then {
+				[] call tmr_autorest_fnc_unrestWeapon;
+			};
+
+			// Deploy the weapon
+			[] call tmr_autorest_fnc_deployWeapon;
+
+			// Spawn a watcher to undeploy if we're no longer aligned or if we move
+			[] spawn {
+				while {player getVariable ["tmr_autorest_deployed", false]} do {
+					sleep 0.5;
+					_playerAnimState = animationState player;
+
+					// Check if the bipod area intersects with something or the ground.
+					_frontCheck = lineIntersectsWith [getposASL tmr_autorest_refPoint7, getposASL tmr_autorest_refPoint8, tmr_autorest_refPoint7, tmr_autorest_refPoint8];
+					_rearCheck = lineIntersectsWith [getposASL tmr_autorest_refPoint8, getposASL tmr_autorest_refPoint9, tmr_autorest_refPoint8, tmr_autorest_refPoint9];
+					_bottomCheck = lineIntersectsWith [getposASL tmr_autorest_refPoint7, getposASL tmr_autorest_refPoint9, tmr_autorest_refPoint7, tmr_autorest_refPoint9];
+					_terrainCheck  = terrainIntersectASL [getposASL tmr_autorest_refPoint7, getposASL tmr_autorest_refPoint9];
+
+					_intersectCheck = (count _frontCheck > 0 || count _rearCheck > 0 || count _bottomCheck > 0 || _terrainCheck);
+
+					// If we intersect with nothing or if we left the deploy animation
+					if (!_intersectCheck || !(["_tmr_deploy", _playerAnimState] call bis_fnc_inString)) then {
+						// Undeploy the weapon.
+						[] call tmr_autorest_fnc_undeployWeapon;
+					};
+				};
+			};
+		};
+	};
+
+	_return;
+};
+
+
+// -------------------------------------------------------------------------------
+// Apply the deployed weapon rest characteristics to the player.
+// -------------------------------------------------------------------------------
+tmr_autorest_fnc_deployWeapon = {
+	if (!tmr_autorest_deployIconDisplayed) then {
+		tmr_autorest_deployIconDisplayed = true;
+		1599 cutRsc ["TMR_Autorest_Deployed", "PLAIN"];
+		((uiNameSpace getVariable "TMR_Autorest_Deployed") displayCtrl 1) ctrlSetFade 0.5;
+		((uiNameSpace getVariable "TMR_Autorest_Deployed") displayCtrl 1) ctrlCommit 0.5;
+	};
+	
+	playSound "tmr_nlaw_plungerPress";
+
+	player setVariable ["tmr_autorest_deployed", true, false];
+
+	_playerAnimState = animationState player;
+	player switchMove format ["%1_tmr_deploy", _playerAnimState];
+	player setUnitRecoilCoefficient TMR_AUTOREST_DEPLOYEDRECOIL;
+};
+
+// -------------------------------------------------------------------------------
+// Remove the deployed weapon rest characteristics from the player.
+// -------------------------------------------------------------------------------
+tmr_autorest_fnc_undeployWeapon = {
+	playSound "tmr_nlaw_plungerRelease";
+
+	player setVariable ["tmr_autorest_deployed", false, false];
+
+	_playerAnimState = animationState player;
+	player switchMove format ["%1", [_playerAnimState, "_tmr_deploy", ""] call CBA_fnc_replace];
+	player setUnitRecoilCoefficient 1;
+
+	if (tmr_autorest_deployIconDisplayed && !tmr_autorest_deployIconTransition) then {
+		[] spawn {
+			tmr_autorest_deployIconTransition = true;
+			((uiNameSpace getVariable "TMR_Autorest_Deployed") displayCtrl 1) ctrlSetFade 1;
+			((uiNameSpace getVariable "TMR_Autorest_Deployed") displayCtrl 1) ctrlCommit 0.14;
+			sleep 0.14;
+			tmr_autorest_deployIconDisplayed = false;
+			tmr_autorest_deployIconTransition = false;
+		};
+	};
+};
+
+// -------------------------------------------------------------------------------
+// Apply the weapon rest characteristics to the player.
+// -------------------------------------------------------------------------------
+tmr_autorest_fnc_restWeapon = {
+	if (!tmr_autorest_restIconDisplayed) then {
+		tmr_autorest_restIconDisplayed = true;
+		1598 cutRsc ["TMR_Autorest_Rested", "PLAIN"];
+		((uiNameSpace getVariable "TMR_Autorest_Rested") displayCtrl 1) ctrlSetFade 0.5;
+		((uiNameSpace getVariable "TMR_Autorest_Rested") displayCtrl 1) ctrlCommit 0.5;
+	};
+	player setUnitRecoilCoefficient TMR_AUTOREST_RESTEDRECOIL ;
+	player setVariable ["tmr_autorest_rested", true, false];
+};
+
+
+// -------------------------------------------------------------------------------
+// Remove the weapon rest characteristics from the player.
+// -------------------------------------------------------------------------------
+tmr_autorest_fnc_unrestWeapon = {
+	player setUnitRecoilCoefficient 1;
+	player setVariable ["tmr_autorest_rested", false, false];
+	if (tmr_autorest_restIconDisplayed && !tmr_autorest_restIconTransition) then {
+		[] spawn {
+			tmr_autorest_restIconTransition = true;
+			((uiNameSpace getVariable "TMR_Autorest_Rested") displayCtrl 1) ctrlSetFade 1;
+			((uiNameSpace getVariable "TMR_Autorest_Rested") displayCtrl 1) ctrlCommit 0.14;
+			sleep 0.14;
+			tmr_autorest_restIconDisplayed = false;
+			tmr_autorest_restIconTransition = false;
+		};
+	};
+};
+
+// -------------------------------------------------------------------------------
+// Create our geometry references.
+// -------------------------------------------------------------------------------
+tmr_autorest_fnc_createRefPoints = {
+	_model = "TMR_Autorest_GeoRef";
+	// _model = "Sign_Sphere10cm_F";
+
+	tmr_autorest_refPoint1 = _model createVehicle position player; 
+	tmr_autorest_refPoint2 = _model createVehicle position player;  
+
+	tmr_autorest_refPoint3 = _model createVehicle position player;  
+	tmr_autorest_refPoint4 = _model createVehicle position player;  
+
+	tmr_autorest_refPoint5 = _model createVehicle position player;  
+	tmr_autorest_refPoint6 = _model createVehicle position player;  
+
+	tmr_autorest_refPoint7 = _model createVehicle position player;  
+	tmr_autorest_refPoint8 = _model createVehicle position player; 
+	tmr_autorest_refPoint9 = _model createVehicle position player; 
+};
+
+// -------------------------------------------------------------------------------
+// Attach our geometry references.
+// -------------------------------------------------------------------------------
+tmr_autorest_fnc_attachRefPoints = {
+	detach tmr_autorest_refPoint1;
+	detach tmr_autorest_refPoint2;
+	detach tmr_autorest_refPoint3;
+	detach tmr_autorest_refPoint4;
+	detach tmr_autorest_refPoint5;
+	detach tmr_autorest_refPoint6;
+	detach tmr_autorest_refPoint7;
+	detach tmr_autorest_refPoint8;
+	detach tmr_autorest_refPoint9;
+
+	tmr_autorest_refPoint1 attachTo [player, [0,0.52,-0.135], "LeftHand"]; 
+	tmr_autorest_refPoint2 attachTo [player, [0,-0.3,-0.40], "RightHand"]; 
+
+	tmr_autorest_refPoint3 attachTo [player, [0.145,0.52,-0.135], "LeftHand"]; 
+	tmr_autorest_refPoint4 attachTo [player, [0.145,-0.3,-0.40], "RightHand"]; 
+
+	tmr_autorest_refPoint5 attachTo [player, [-0.145,0.52,-0.135], "LeftHand"]; 
+	tmr_autorest_refPoint6 attachTo [player, [-0.145,-0.3,-0.40], "RightHand"]; 
+
+	tmr_autorest_refPoint7 attachTo [player, [0,0.38,-0.02], "RightHand"]; 
+	tmr_autorest_refPoint8 attachTo [player, [0,0.24,-0.4], "RightHand"]; 
+	tmr_autorest_refPoint9 attachTo [player, [0,0.57,-0.4], "RightHand"]; 
+};
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+// This PEH monitors the position of the geometry references, determines if the
+// lines between them intersect with an object, then applies the 'rested' state.
+// It also handles unit changes and updates the geo refs.
+
+tmr_autorest_endPEH = false;
+
+// Huge public event handler follows.
+_handle = [
+/* Code */
+{
+	// If the player unit has changed, reattach all the ref points to the new unit.
+	if (_unit != format ["%1", player]) then {
+		player sidechat 'player changed';
+		[] call tmr_autorest_fnc_attachRefPoints;		
+	};
+
+	// Update the current player unit tracker.
+	_unit = format ["%1", player];
+
+	// Player can't be moving very much, must have rifle or launcher, and must be able to fire.
+	
+	_otherChecks = (vehicle player == player && speed player < 1 && (currentWeapon player == primaryWeapon player || currentWeapon player == secondaryWeapon player) && canFire player);
+
+	// Also can't be deployed.
+	if (_otherChecks && !(player getVariable ["tmr_autorest_deployed", false])) then {
+
+		// Check if the weapon geo references intersect with something.
+		_centerCheck = lineIntersectsWith [getposASL tmr_autorest_refPoint1, getposASL tmr_autorest_refPoint2, tmr_autorest_refPoint1, tmr_autorest_refPoint2];
+		_leftCheck = lineIntersectsWith [getposASL tmr_autorest_refPoint3, getposASL tmr_autorest_refPoint4, tmr_autorest_refPoint3, tmr_autorest_refPoint4];
+		_rightCheck = lineIntersectsWith [getposASL tmr_autorest_refPoint5, getposASL tmr_autorest_refPoint6, tmr_autorest_refPoint5, tmr_autorest_refPoint6];
+
+		if (count _centerCheck > 0 || count _leftCheck > 0 || count _rightCheck > 0) then {
+			// Rest the weapon
+			[] call tmr_autorest_fnc_restWeapon;
+		} else {
+			// Unrest the weapon
+			[] call tmr_autorest_fnc_unrestWeapon;
+		};
+	} else {
+		if (player getVariable ["tmr_autorest_rested", false]) then {
+			// Unrest the weapon
+			[] call tmr_autorest_fnc_unrestWeapon;
+		};
+	};
+
+}, 
+/* Parameters */
+[],
+/* Delay */
+0.6,
+/* Initialization */
+{
+	// Get the string of the player
+	// There must be a better way to do this.
+	// Wish I could dereference player.
+	_unit = format ["%1", player];
+
+	[] call tmr_autorest_fnc_createRefPoints;
+	[] call tmr_autorest_fnc_attachRefPoints;
+}, 
+/* On exit, do...*/
+{
+	deletevehicle tmr_autorest_refPoint1; deletevehicle tmr_autorest_refPoint2; deletevehicle tmr_autorest_refPoint3; deletevehicle tmr_autorest_refPoint4; deletevehicle tmr_autorest_refPoint5; deletevehicle tmr_autorest_refPoint6;tmr_autorest_refPoint7; deletevehicle tmr_autorest_refPoint8; deletevehicle tmr_autorest_refPoint9;
+}, 
+/* Run condition */
+{true},
+/* Exit condition */
+{tmr_autorest_endPEH}, 
+/* Private variables */
+["_unit"]
+] call cba_common_fnc_addPerFrameHandlerLogic;
+
+/////////////////////////////////////////////////////////////////////////////////
+ 
+// Add key handler. 
+[] spawn {
+	waituntil {!isNull (findDisplay 46)};
+	(findDisplay 46) displayAddEventHandler ["KeyDown", "_this call tmr_autorest_fnc_deployKeyDownEH"];
+};
+
+/////////////////////////////////////////////////////////////////////////////////
+
+// Initialization complete.
+
+tmr_autorest = true;
