00ae8f21c30a8f480917eef51d48d5bef37eace9
Taosenai <schultz.ryan@gmail.com>
2013-07-31 16:27:49 -0400

    Add language module (empty)

---
+++ tmr_language/config.cpp
@@ -0,0 +1,26 @@
+#define true	1
+#define false	0
+
+class CfgPatches {
+	class tmr_language {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.60;
+		requiredAddons[] = {A3_Weapons_F, A3_Weapons_F_beta, A3_Weapons_F_Acc, A3_Weapons_F_Beta_Acc, A3_Characters_F, A3_Language_F};
+		version = 0.1;
+		author[] = {"Taosenai"};
+		authorUrl = "http://www.ryanschultz.org/tmr/";
+	};
+};
+
+class CfgMods {
+	class tmr_language {
+		dir = "tmr_language";
+		name = "TMR: Language Module";
+		picture = "";
+		hidePicture = "true";
+		hideName = "true";
+		actionName = "Website";
+		action = "http://www.ryanschultz.org/tmr/";
+	};
+};
