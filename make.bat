96f19eb0281a4cddb06c992a3592e23df67cc470
Taosenai <schultz.ryan@gmail.com>
2013-08-02 19:06:45 -0400

    Update make.bat to support VS

--- make.bat
+++ make.bat
@@ -9,6 +9,10 @@
 set releaseFolder=@tmr
 :: Modules to build
 set modules=tmr_core,tmr_nlaw,tmr_rpg42,tmr_disposable,tmr_u_loadouts,tmr_aimsway,tmr_autorest,tmr_smallarms,tmr_smallarms_suppressors,tmr_smallarms_recoil,tmr_smallarms_ugl,tmr_language
+:: Visual Studio solutions to build
+set VSsolutions=dll_configwriter\tmr_configwriter.sln
+:: Visual Studio output files to copy to release
+set VSoutputs=dll_configwriter\Release\tmr_configwriter.dll
 :: BI work drive
 set workPath=P:\
 :: Location of the BinPBO utility.
@@ -23,6 +27,9 @@ set DoSign="YES"
 set DSSignFile="F:\Workspace\tmr_sign\DSSignFile.exe"
 :: Path to private key
 set PrivateKey="F:\Workspace\tmr_sign\TMR.biprivatekey"
+:: Path to Visual Studio vcvarsall.bat file to set up MSBuild environment
+set VSVarsPath="F:\Installed Applications\Visual Studio Professional 2012\VC\vcvarsall.bat"
+
 
 
 set releaseVersion=%2
@@ -36,12 +43,18 @@ set releaseDir=%CD%\release\%releaseFolder%\Addons\
 
 mkdir %releaseDir%
 
+IF "%1"=="nopbo" GOTO :Nopbo 
+
 FOR %%A IN (%modules%) DO (
 	set currentModule=%%A
 	CALL :Build
 )
 
 CALL :Cleanup
+
+:Nopbo
+
+IF NOT "%1"=="novs" CALL :VSMake
 
 IF "%1"=="release" CALL :Release
 IF "%1"=="test" CALL :Test
@@ -52,18 +65,18 @@ GOTO :End
 
 :: Sub to build a module
 :Build
-ECHO ###############################
+ECHO ###############################################################
 ECHO Building %currentModule%...
-ECHO ###############################
+ECHO ###############################################################
 
 ECHO Copying files to %workPath% ...
 rmdir %workPath%%currentModule% /S /Q
 robocopy %currentModule% %workPath%%currentModule% /E /njh /njs /ndl /nc /ns /NFL
 
 ECHO BinPBOing %currentModule%...
-ECHO -------------------------------
+ECHO ---------------------------------------------------------------
 %BinPBO% %workPath%%currentModule% %releaseDir% -CLEAR
-ECHO -------------------------------
+ECHO ---------------------------------------------------------------
 ECHO:
 ECHO Build of %currentModule% complete!
 
@@ -94,9 +107,71 @@ GOTO :EOF
 
 :: Sub to cleanup log files
 :Cleanup
-ECHO All builds complete, cleaning up...
+ECHO All modules complete, cleaning up...
 del %releaseDir%*.log
 GOTO :EOF
 
+:: Sub to build all VS stuff
+:VSMake
+
+ECHO:
+ECHO -------------------------------
+ECHO Making Visual Studio files...
+ECHO -------------------------------
+ECHO:
+ECHO Setting up environment...
+
+CALL %VSVarsPath% x86
+
+ECHO:
+ECHO Building all VS solutions...
+ECHO:
+
+
+FOR %%A IN (%VSsolutions%) DO (
+	set currentVSSolution=%%A
+	CALL :VSBuild
+)
+
+ECHO ----------------------------------------------------------------
+
+FOR %%A IN (%VSoutputs%) DO (
+	set currentVSoutput=%%A
+	CALL :VSOutput
+)
+
+ECHO:
+ECHO ----------------------------------------------------------------
+ECHO Done building Visual Studio files!
+ECHO ----------------------------------------------------------------
+ECHO:
+
+GOTO :EOF
+
+
+:: Sub to build a a VS solution
+:VSBuild
+ECHO ###############################################################
+ECHO Building %currentVSSolution%...
+ECHO ###############################################################
+
+MSBuild %currentVSSolution%
+
+GOTO :EOF
+
+:: Sub to copy VS output
+:VSOutput
+ECHO Copying %currentVSSolution% to release...
+
+copy %currentVSoutput% release\
+
+ECHO:
+
+GOTO :EOF
+
+
 :End
+ECHO:
+ECHO Build complete.
+ECHO:
 PAUSE
