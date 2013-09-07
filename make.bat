7236f5febcd4e40ff66dcfe0dd7db33acfb1d0ac
Taosenai <schultz.ryan@gmail.com>
2013-09-07 15:32:07 -0400

    Update make.bat

--- make.bat
+++ make.bat
@@ -8,7 +8,7 @@
 :: Name of the release
 set releaseFolder=@tmr
 :: Modules to build
-set modules=tmr_core,tmr_nlaw,tmr_rpg42,tmr_disposable,tmr_u_loadouts,tmr_aimsway,tmr_autorest,tmr_smallarms,tmr_smallarms_suppressors,tmr_smallarms_recoil,tmr_smallarms_ugl,tmr_language,tmr_optics,tmr_ballistics
+set modules=tmr_core,tmr_nlaw,tmr_rpg42,tmr_disposable,tmr_u_loadouts,tmr_aimsway,tmr_autorest,tmr_smallarms,tmr_smallarms_suppressors,tmr_smallarms_suppressors_unique,tmr_smallarms_suppressors_recoil,tmr_smallarms_recoil,tmr_smallarms_ugl,tmr_language,tmr_optics,tmr_ballistics
 :: Disabled: tmr_core_configwriter,tmr_core_binds,tmr_optics_nonfree
 :: Visual Studio solutions to build
 ::set VSsolutions=dll_configwriter\tmr_configwriter.sln
