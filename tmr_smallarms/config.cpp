41c4bb58f974667de1805cb5b7e24d3e052b5869
Ryan <schultz.ryan@gmail.com>
2013-09-18 19:36:20 -0400

    smallarms: Renames and fixes to BI stuff

--- tmr_smallarms/config.cpp
+++ tmr_smallarms/config.cpp
@@ -49,7 +49,7 @@ class CfgMagazines {
 	// 5.56mm ////////////////////////////////////
 
 	class 30Rnd_556x45_Stanag : CA_Magazine {
-		descriptionshort = "Caliber: 5.56x45mm<br />Rounds: 30<br />Used in: TRG-20, TRG-21, Mk20, SDAR";
+		descriptionshort = "Caliber: 5.56x45mm<br />Rounds: 30<br />Type: STANAG";
 		displayname = "5.56mm 30Rnd Mag";
 		displaynameshort = "5.56mm";
 
@@ -58,25 +58,25 @@ class CfgMagazines {
 	};
 
 	class 30Rnd_556x45_Stanag_Tracer_Red: 30Rnd_556x45_Stanag {
-		descriptionshort = "Caliber: 5.56x45mm Tracer (Red)<br />Rounds: 30<br />Used in: TRG-20, TRG-21, Mk20, SDAR";
+		descriptionshort = "Caliber: 5.56x45mm Tracer (Red)<br />Rounds: 30<br />Type: STANAG";
 		displayname = "5.56mm 30Rnd Tracer Mag";
 		displaynameshort = "5.56mm";
 	};
 
 	class 30Rnd_556x45_Stanag_Tracer_Green: 30Rnd_556x45_Stanag {
-		descriptionshort = "Caliber: 5.56x45mm Tracer (Green)<br />Rounds: 30<br />Used in: TRG-20, TRG-21, Mk20, SDAR";
+		descriptionshort = "Caliber: 5.56x45mm Tracer (Green)<br />Rounds: 30<br />Type: STANAG";
 		displayname = "5.56mm 30Rnd Tracer Mag";
 		displaynameshort = "5.56mm";
 	};
 
 	class 30Rnd_556x45_Stanag_Tracer_Yellow: 30Rnd_556x45_Stanag {
-		descriptionshort = "Caliber: 5.56x45mm Tracer (Yellow)<br />Rounds: 30<br />Used in: TRG-20, TRG-21, Mk20, SDAR";
+		descriptionshort = "Caliber: 5.56x45mm Tracer (Yellow)<br />Rounds: 30<br />Type: STANAG";
 		displayname = "5.56mm 30Rnd Tracer Mag";
 		displaynameshort = "5.56mm";
 	};
 
 	class 20Rnd_556x45_UW_mag: 30Rnd_556x45_Stanag {
-		descriptionshort = "Caliber: 5.56x45mm MEA Supercavitating<br />Rounds: 20<br />Used in: SDAR";
+		descriptionshort = "Caliber: 5.56x45mm MEA Supercavitating<br />Rounds: 20<br />Type: SDAR";
 		displayname = "5.56mm 20Rnd MEA Mag";
 		displaynameshort = "5.56mm MEA";
 	};
@@ -84,8 +84,8 @@ class CfgMagazines {
 	// 6.5mm //////////////////////////////////////////
 
 	class 30Rnd_65x39_caseless_mag : CA_Magazine { // MX!!
-		descriptionshort = "Caliber: 6.5x39mm Caseless<br />Rounds: 30<br />Used in: MX-1";
-		displayname = "6.5mm 30Rnd MX Mag";
+		descriptionshort = "Caliber: 6.5x39mm Caseless<br />Rounds: 30<br />Type: MX";
+		displayname = "6.5mm 30Rnd Mag";
 		displaynameshort = "6.5mm";
 
 		tracersEvery = 0;
@@ -93,14 +93,14 @@ class CfgMagazines {
 	};
 
 	class 30Rnd_65x39_caseless_mag_Tracer : 30Rnd_65x39_caseless_mag { // MX!!
-		descriptionshort = "Caliber: 6.5x39mm Caseless Tracer (Red)<br />Rounds: 30<br />Used in: MX-1";
-		displayname = "6.5mm 30Rnd MX Tracer Mag";
+		descriptionshort = "Caliber: 6.5x39mm Caseless Tracer (Red)<br />Rounds: 30<br />Type: KT2002";
+		displayname = "6.5mm 30Rnd Tracer Mag";
 		displaynameshort = "6.5mm Tracer";
 	};
 
 	class 30Rnd_65x39_caseless_green : 30Rnd_65x39_caseless_mag { // Katiba!!
-		descriptionshort = "Caliber: 6.5x39mm Caseless<br />Rounds: 100<br />Used in: Katiba KT-2002";
-		displayname = "6.5mm 30Rnd Katiba Mag";
+		descriptionshort = "Caliber: 6.5x39mm Caseless<br />Rounds: 100<br />Type: KT2002";
+		displayname = "6.5mm 30Rnd Mag";
 		displaynameshort = "6.5mm";
 
 		tracersEvery = 0;
@@ -108,14 +108,14 @@ class CfgMagazines {
 	};
 
 	class 30Rnd_65x39_caseless_green_Tracer : 30Rnd_65x39_caseless_green { // Katiba!!
-		descriptionshort = "Caliber: 6.5x39mm Caseless Tracer (Green)<br />Rounds: 100<br />Used in: Katiba KT-2002";
-		displayname = "6.5mm 30Rnd Katiba Tracer Magazine";
+		descriptionshort = "Caliber: 6.5x39mm Caseless Tracer (Green)<br />Rounds: 100<br />Type: KT2002";
+		displayname = "6.5mm 30Rnd Tracer Magazine";
 		displaynameshort = "6.5mm Tracer";
 	};
 
 	class 100Rnd_65x39_caseless_mag : CA_Magazine {
-		descriptionshort = "Caliber: 6.5x39mm Caseless<br />Rounds: 100<br />Used in: MX-1";
-		displayname = "6.5mm 100Rnd MX Mag";
+		descriptionshort = "Caliber: 6.5x39mm Caseless<br />Rounds: 100<br />Type: MX";
+		displayname = "6.5mm 100Rnd Mag";
 		displaynameshort = "6.5mm";
 
 		tracersEvery = 5;
@@ -123,13 +123,13 @@ class CfgMagazines {
 	};
 
 	class 100Rnd_65x39_caseless_mag_Tracer : 100Rnd_65x39_caseless_mag {
-		descriptionshort = "Caliber: 6.5x39mm Caseless Tracer (Red)<br />Rounds: 100<br />Used in: MX-1";
-		displayname = "6.5mm 100Rnd MX Tracer Mag";
+		descriptionshort = "Caliber: 6.5x39mm Caseless Tracer (Red)<br />Rounds: 100<br />Type: MX";
+		displayname = "6.5mm 100Rnd Tracer Mag";
 		displaynameshort = "6.5mm Tracer";
 	};
 
 	class 200Rnd_65x39_cased_Box : 100Rnd_65x39_caseless_mag {
-		descriptionshort = "Caliber: 6.5x39mm Caseless<br />Rounds: 200<br />Used in: Mk200 MG";
+		descriptionshort = "Caliber: 6.5x39mm Caseless<br />Rounds: 200<br />Type: M20 link";
 		displayname = "6.5mm 200Rnd Box";
 		displaynameshort = "6.5mm";
 
@@ -137,7 +137,7 @@ class CfgMagazines {
 		lastRoundsTracer = 3;
 	};
 	class 200Rnd_65x39_cased_Box_Tracer: 200Rnd_65x39_cased_Box {
-		descriptionshort = "Caliber: 6.5x39mm Caseless Tracer (Green)<br />Rounds: 200<br />Used in: Mk200 MG";
+		descriptionshort = "Caliber: 6.5x39mm Caseless Tracer (Green)<br />Rounds: 200<br />Type: M20 link";
 		displayname = "6.5mm 200Rnd Tracer Box";
 		displaynameshort = "6.5mm Tracer";
 	};
@@ -145,13 +145,13 @@ class CfgMagazines {
 	// 7.62mm //////////////////////////////////////////
 
 	class 20Rnd_762x51_Mag: CA_Magazine {
-		descriptionshort = "Caliber: 7.62x51mm<br />Rounds: 20<br />Used in: Mk18 ABR";
+		descriptionshort = "Caliber: 7.62x51mm<br />Rounds: 20<br />Type: M14";
 		displayname = "7.62mm 20Rnd Mag";
 		displaynameshort = "7.62mm";
 	};
 
 	class 150Rnd_762x51_Box : CA_Magazine {
-		descriptionshort = "Caliber: 7.62x51mm<br/>Rounds: 150<br />Used in: Zafir ZG7";
+		descriptionshort = "Caliber: 7.62x51mm<br/>Rounds: 150<br />Type: M13 link";
 		displayname = "7.62mm 150Rnd Box";
 
 		tracersEvery = 5;
@@ -166,19 +166,19 @@ class CfgMagazines {
 	// Anti-materiel ///////////////////////////////
 
 	class 7Rnd_408_Mag: CA_Magazine {
-		descriptionshort = "Caliber: .408 Cheetah<br />Rounds: 7<br />Used in: M320 LRR";
+		descriptionshort = "Caliber: .408 CheyTac<br />Rounds: 7<br />Type: CheyTac";
 		displayname = ".408 7Rnd Mag";
 	};
 
 	class 5Rnd_127x108_Mag: CA_Magazine {
-		descriptionshort = "Caliber: 12.7x108mm APDS<br />Rounds: 5<br />Used in: GM6 Lynx";
+		descriptionshort = "Caliber: 12.7x108mm APDS<br />Rounds: 5<br />Type: Gepárd";
 		displayname = "12.7mm 5Rnd Mag";
 	};
 
 	// SMG & Pistol ////////////////////////////
 
 	class 30Rnd_9x21_Mag : CA_Magazine {
-		descriptionshort = "Caliber: 9x19mm<br />Rounds: 30<br />Used in: Scorpion EVO-4, PDW2000, P07, Rook-40, ACP-C2";
+		descriptionshort = "Caliber: 9x19mm<br />Rounds: 30<br />Type: Scorpion";
 		displayname = "9mm 30Rnd Mag";
 		displaynameshort = "9mm";
 
@@ -186,31 +186,37 @@ class CfgMagazines {
 	};
 
 	class 16Rnd_9x21_Mag: 30Rnd_9x21_Mag {
-		descriptionshort = "Caliber: 9x19mm<br />Rounds: 16<br />Used in: PDW2000, P07, Rook-40, ACP-C2";
-		displayname = "9mm 16Rnd Mag";
+		descriptionshort = "Caliber: 9x19mm<br />Rounds: 17<br />Type: Glock";
+		displayname = "9mm 17Rnd Mag";
 		displaynameshort = "9mm";
+
+		count = 17;
 	};
 
 	class 30Rnd_45ACP_Mag_SMG_01 : 30Rnd_9x21_Mag {
-		descriptionshort = "Caliber: .45 ACP<br />Rounds: 30<br />Used in: Vermin SMG";
-		displayname = ".45 30Rnd Mag";
+		descriptionshort = "Caliber: .45 ACP<br />Rounds: 25<br />Type: Glock";
+		displayname = ".45 25Rnd Mag";
 		displaynameshort = ".45";
+
+		picture = "\A3\weapons_f\data\ui\M_30Rnd_9x21_CA.paa";
+
+		count = 25;
 
 		tracersEvery = 0;
 		lastRoundsTracer = 0;
 	};
 
 	class 9Rnd_45ACP_Mag : 30Rnd_45ACP_Mag_SMG_01 {
-		descriptionshort = "Caliber: .45 ACP<br />Rounds: 9<br />Used in: ACP-C2";
-		displayname = ".45 9Rnd Mag";
+		descriptionshort = "Caliber: .45 ACP<br />Rounds: 8<br />Type: 1911";
+		displayname = ".45 8Rnd Mag";
 		displaynameshort = ".45";
 
-		count = 9;
+		count = 8;
 	};
 
 	class 30Rnd_45ACP_Mag_SMG_01_Tracer_Green: 30Rnd_45ACP_Mag_SMG_01 {
-		descriptionshort = "Caliber: .45 ACP Tracer (Green)<br />Rounds: 30<br />Used in: Vermin SMG";
-		displayname = ".45 30Rnd Tracer Mag";
+		descriptionshort = "Caliber: .45 ACP Tracer (Green)<br />Rounds: 25<br />Type: Glock";
+		displayname = ".45 25Rnd Tracer Mag";
 	};
 };
 
@@ -243,17 +249,22 @@ class CfgWeapons {
 	class arifle_MX_Base_F : Rifle_Base_F {
 		magazines[] = {"30Rnd_65x39_caseless_mag", "30Rnd_65x39_caseless_mag_Tracer", "100Rnd_65x39_caseless_mag", "100Rnd_65x39_caseless_mag_Tracer"};
 
-		displayName = "MX";
-		descriptionShort = "Assault Rifle<br />Caliber: 6.5x39mm";
+		// http://www.bushmaster.com/acr/#/intro
+		// 800 rpm (whatever, fictional lol)
 
-		class Single : Mode_SemiAuto {};
+		//class Single : Mode_SemiAuto {};
+		class Single : Mode_SemiAuto {
+			reloadTime = 0.075;
+		};
+
+		class FullAuto: Mode_FullAuto {
+			reloadTime = 0.075;
+		};
 	};
 
 	class arifle_MX_F : arifle_MX_Base_F {
 		displayName = "MX";
 		descriptionShort = "Assault Rifle<br />Caliber: 6.5x39mm";
-
-
 	};
 
 	class arifle_MXC_F : arifle_MX_Base_F {
@@ -263,11 +274,11 @@ class CfgWeapons {
 
 
 	class arifle_MX_GL_F : arifle_MX_Base_F {
-		displayName = "MX 3GL-H";
+		displayName = "MX 3GL";
 		descriptionShort = "Assault Rifle<br />Caliber: 6.5x39mm";
 
 		// class GL_3GL_F : UGL_F {
-		// 	displayName = "3GL-H";
+		// 	displayName = "3GL";
 		// 	descriptionShort = "Grenade Launcher";
 		// };
 	};
@@ -289,17 +300,25 @@ class CfgWeapons {
 	// Katibas ////////////////////////////////////////////////////
 
 	class arifle_katiba_Base_F : Rifle_Base_F {
-		displayName = "Katiba KT-2002";
-		descriptionShort = "Assault Rifle<br />Caliber: 6.5x39mm";
+		// http://world.guns.ru/assault/iran/khaybar-kh2002-e.html
+		// 800 rpm
+
+		class Single : Mode_SemiAuto {
+			reloadTime = 0.075;
+		};
+
+		class FullAuto: Mode_FullAuto {
+			reloadTime = 0.075;
+		};
 	};
 
 	class arifle_Katiba_F : arifle_katiba_Base_F {
-		displayName = "Katiba KT-2002";
+		displayName = "KT2002 Katiba";
 		descriptionShort = "Assault Rifle<br />Caliber: 6.5x39mm";
 	};
 
 	class arifle_Katiba_GL_F : arifle_katiba_Base_F {
-		displayName = "Katiba KT-2002 KGL";
+		displayName = "KT2002 Katiba KGL";
 		descriptionShort = "Assault Rifle<br />Caliber: 6.5x39mm";
 
 		//class EGLM : UGL_F {
@@ -309,36 +328,43 @@ class CfgWeapons {
 	};
 
 	class arifle_Katiba_C_F : arifle_katiba_Base_F {
-		displayName = "Katiba KT-2002C";
-		descriptionShort = "Carbine<br />Caliber: 6.5x39mm";
+		displayName = "KT2002C Katiba";
+		descriptionShort = "Assault Rifle<br />Caliber: 6.5x39mm";
 	};
 
 	// SDAR //////////////////////////////////////////////////
 
 	class SDAR_base_F : Rifle_Base_F {
-		displayName = "SDAR";
-		descriptionShort = "Amphibious Rifle<br />Caliber: 5.56x45mm";
+		modes[] = {"Single", "FullAuto"}; // Leave in the imaginary full-auto mode because, whatever.
 	};
 
 	class arifle_SDAR_F : SDAR_base_F {
-		displayName = "SDAR";
+		displayName = "CFB SDAR";
 		descriptionShort = "Amphibious Rifle<br />Caliber: 5.56x45mm";
+
 	};
 
 	// Tavor TRG ////////////////////////////////////////////////
 
 	class Tavor_base_F : Rifle_Base_F {
-		displayName = "TRG-21";
-		descriptionShort = "Assault Rifle<br />Caliber: 5.56x45mm";
+		// http://www.israel-weapon.com/files/brochure_2012/IWI_TAVOR_AR.pdf
+		// 700 ~1000 rpm (850)
+		class Single : Mode_SemiAuto {
+			reloadTime = 0.07;
+		};
+
+		class FullAuto: Mode_FullAuto {
+			reloadTime = 0.07;
+		};
 	};
 
 	class arifle_TRG21_F : Tavor_base_F {
-		displayName = "TRG-21";
+		displayName = "TAR-21";
 		descriptionShort = "Assault Rifle<br />Caliber: 5.56x45mm";
 	};
 
 	class arifle_TRG21_GL_F : arifle_TRG21_F {
-		displayName = "TRG-21 EGLM";
+		displayName = "TAR-21 EGLM";
 		descriptionShort = "Assault Rifle<br />Caliber: 5.56x45mm";
 
 		// class EGLM : UGL_F {
@@ -348,29 +374,36 @@ class CfgWeapons {
 	};
 
 	class arifle_TRG20_F : Tavor_base_F {
-		displayName = "TRG-20";
-		descriptionShort = "Carbine<br />Caliber: 5.56x45mm"; // It's not really a carbine...
+		displayName = "CTAR-21";
+		descriptionShort = "Assault Rifle<br />Caliber: 5.56x45mm";
 	};
 
 	// Mk20 (F2000) //////////////////////////////////////////////////
 
 	class mk20_base_F : Rifle_Base_F {
-		displayName = "Mk20";
-		descriptionShort = "Assault Rifle<br />Caliber: 5.56x45mm";
+		// http://www.fnherstal.com/primary-menu/products-capabilities/rifles/technical-data/product/182/232/182/1/_/fn-f2000R-standard.html
+		// 850 rpm
+		class Single : Mode_SemiAuto {
+			reloadTime = 0.07;
+		};
+
+		class FullAuto: Mode_FullAuto {
+			reloadTime = 0.07;
+		};
 	};
 
 	class arifle_Mk20_F : mk20_base_F {
-		displayName = "Mk20";
+		displayName = "F2000A";
 		descriptionShort = "Assault Rifle<br />Caliber: 5.56x45mm";
 	};
 
 	class arifle_Mk20C_F : mk20_base_F {
-		displayName = "Mk20C";
-		descriptionShort = "Carbine<br />Caliber: 5.56x45mm"; // It's not really a carbine...
+		displayName = "F2000A Tactical";
+		descriptionShort = "Assault Rifle<br />Caliber: 5.56x45mm";
 	};
 
 	class arifle_Mk20_GL_F : mk20_base_F {
-		displayName = "Mk20 EGLM";
+		displayName = "F2000A EGLM";
 		descriptionShort = "Assault Rifle<br />Caliber: 5.56x45mm";
 
 		// class EGLM : UGL_F {
@@ -382,43 +415,92 @@ class CfgWeapons {
 	// SMG Vermin ////////////////////////////////////////////////////
 
 	class SMG_01_Base : Rifle_Base_F {
-		displayName = "Vermin SMG";
-		descriptionShort = "Submachine gun<br />Caliber: .45 ACP";
+		displayName = "Vector SMG";
+		descriptionShort = "Submachine Gun<br />Caliber: .45 ACP";
+
+		// http://kriss-usa.com/pdf/operatormanual/
+		// 1200 rpm
+		class Single : Mode_SemiAuto {
+			reloadTime = 0.05;
+		};
 
 		class Burst : Mode_Burst {
 			burst = 2;
+			reloadTime = 0.05;
+		};
+
+		class FullAuto: Mode_FullAuto {
+			reloadTime = 0.05;
 		};
 	};
 
 	class SMG_01_F : SMG_01_Base {
-		displayName = "Vermin SMG";
-		descriptionShort = "Submachine gun<br />Caliber: .45 ACP";
+		displayName = "Vector SMG";
+		descriptionShort = "Submachine Gun<br />Caliber: .45 ACP";
 		
 	};
 
 	// SMG Scorpion ////////////////////////////////////////////////////
 
 	class SMG_02_base_F : Rifle_Base_F {
-		displayName = "Scorpion EVO-4";
-		descriptionShort = "Submachine Gun<br />Caliber: 9x19mm";
+		//http://www.czub.cz/zbrojovka/cz-manual/Instruction-Manual-Scorpion-EVO-3-A1_en.pdf
+		// 1150 rpm
+
+		class Single : Mode_SemiAuto {
+			// http://www.stengg.com/upload/915fGdhTi3ggnnGQGGL.pdf
+			// 900-1100 rpm (1000rpm)
+			reloadTime = 0.052;
+		};
+
+		class Burst : Mode_Burst {
+			reloadTime = 0.052;
+		};
+
+		class FullAuto: Mode_FullAuto {
+			reloadTime = 0.052;
+		};
 	};	
 
 	class SMG_02_F : SMG_02_base_F {
-		displayName = "Scorpion EVO-4";
+		displayName = "Scorpion Evo 3A1";
 		descriptionShort = "Submachine Gun<br />Caliber: 9x19mm";
+	};
+
+	// SMG PDW2000 ///////////////////////////////////////////////////
+
+	class pdw2000_base_F: Rifle_Base_F {
+		magazines[] = {"30Rnd_9x21_Mag"};
+
+		modes[] = {"Single", "FullAuto"}; // No burst on this thing
+
+		class Single : Mode_SemiAuto {
+			// http://www.stengg.com/upload/915fGdhTi3ggnnGQGGL.pdf
+			// 900-1100 rpm (1000rpm)
+			reloadTime = 0.06;
+		};
+
+		class FullAuto: Mode_FullAuto {
+			reloadTime = 0.06;
+		};
+	};
+	class hgun_pdw2000_F : pdw2000_base_F {
+		displayName = "CPW-CZ";
+		descriptionShort = "Personal Defense Weapon<br />Caliber: 9x19mm";
 	};
 
 	// Pistols //////////////////////////////////////////////
 
 	class Pistol_Base_F;
 	class hgun_P07_F : Pistol_Base_F {
-		displayName = "P07";
+		displayName = "P99G";
 		descriptionShort = "Handgun<br />Caliber: 9x19mm";
+		magazines[] = {"16Rnd_9x21_Mag"};
 	};
 
 	class hgun_Rook40_F : Pistol_Base_F {
-		displayName = "Rook-40";
+		displayName = "MP-443G";
 		descriptionShort = "Handgun<br />Caliber: 9x19mm";
+		magazines[] = {"16Rnd_9x21_Mag"};
 	};
 
 	class hgun_ACPC2_F : Pistol_Base_F {
@@ -431,67 +513,71 @@ class CfgWeapons {
 	class Rifle_Long_Base_F;
 
 	class LMG_Mk200_F : Rifle_Long_Base_F {
-		displayName = "Mk200 MG";
+		displayName = "Mk200 LMG";
 		descriptionShort = "Machine Gun<br />Caliber: 6.5x39mm";
 
-		modes[] = {"manual", "single", "close", "short", "medium", "far_optic1", "far_optic2"};
-		class Single : Mode_SemiAuto {
-			// SOUND MOD ALERT
-			reloadTime = 0.075;
-			dispersion = 0.00093;
-			// Recoil is defined in tmr_smallarms_recoil
-			begin1[] = {"A3\sounds_f\weapons\M200\Mk200_st_4a", 1.25893, 1, 1200};
-			begin2[] = {"A3\sounds_f\weapons\M200\Mk200_st_5a", 1.25893, 1, 1200};
-			begin3[] = {"A3\sounds_f\weapons\M200\Mk200_st_6a", 1.25893, 1, 1200};
-			soundBegin[] = {"begin1", 0.34, "begin2", 0.33, "begin3", 0.33};
-			weaponSoundEffect = "DefaultRifle";
-			closure1[] = {"A3\sounds_f\weapons\closure\sfx7", 1.41254, 1, 40};
-			closure2[] = {"A3\sounds_f\weapons\closure\sfx8", 1.41254, 1, 40};
-			soundClosure[] = {"closure1", 0.5, "closure2", 0.5};
-			soundContinuous = 0;
-			soundBurst = 0;
-			minRange = 0;
-			minRangeProbab = 0.3;
-			midRange = 3;
-			midRangeProbab = 0.7;
-			maxRange = 5;
-			maxRangeProbab = 0.04;
+		modes[] = {"manual", "Single", "close", "short", "medium", "far_optic1", "far_optic2"};
+
+		class manual : Mode_FullAuto {
+			// http://www.defensereview.com/kac-stoner-lmg-belt-fed-5-56mm-nato-lightweight-light-machine-gun-squad-automatic-weapon-lmgsaw-displayed-at-sofic-2010/
+			// 550 rpm
+			reloadTime = 0.109;
+		};
+
+		// Add semi-auto mode.
+		// Inherit from 'manual' for sound reasons.
+		class Single : manual {
+			reloadTime = 0.109;
+			dispersion = 0.00175; // radians. Equal to 6 MOA.
+			autofire = 0;
+			burst = 1;
+			displayname = "Semi";
+			texturetype = "semi";
 			showToPlayer = true;
 		};
 	};
 
 	class LMG_Zafir_F: Rifle_Long_Base_F {
-		displayName = "Zafir ZG7 MG";
+		displayName = "Negev NG7";
 		descriptionShort = "Machine Gun<br />Caliber: 7.62x51mm";
 
 		modes[] = {"FullAuto", "Single",  "close", "short", "medium", "far_optic1", "far_optic2"};
+
+		class FullAuto : Mode_FullAuto {
+			reloadTime = 0.070; // 850 RPM on gas position 1
+		};
 	};
 
 	// Sniper and anti-materiel rifles /////////////////////////////////
 
 	class EBR_base_F : Rifle_Long_Base_F {
-		displayName = "Mk18 ABR";
-		descriptionShort = "Marksman/Battle Rifle<br />Caliber: 7.62x51mm";
+		// EMR/EBR is typically issued semi-auto AFAIK
+		modes[] = {"Single", "single_close_optics1", "single_medium_optics1", "single_far_optics1"};
+		cursor = "arifle";
 	};
 
 	class srifle_EBR_F : EBR_base_F {
-		displayName = "Mk18 ABR";
+		displayName = "Mk14 Mod 1 EBR";
 		descriptionShort = "Marksman/Battle Rifle<br />Caliber: 7.62x51mm";
 	};
 
 	class LRR_base_F : Rifle_Long_Base_F {
-		displayName = "M320 LRR";
-		descriptionShort = "Anti-materiel Rifle<br />Caliber: .408 Cheetah";
+		cursor = "arifle";
 	};
 
 	class srifle_LRR_F : LRR_base_F {
-		displayName = "M320 LRR";
-		descriptionShort = "Anti-materiel Rifle<br />Caliber: .408 Cheetah";
+		displayName = "M200 Intervention";
+		descriptionShort = "Anti-materiel Rifle<br />Caliber: .408 CheyTac";
 	};
 
 	class GM6_base_F : Rifle_Long_Base_F {
-		displayName = "GM6 Lynx";
-		descriptionShort = "Anti-materiel Rifle<br />Caliber: 12.7x108mm";
+		cursor = "arifle";
+
+		// Fuck your balancing, BI.
+		class Single : Mode_SemiAuto {
+			// 250 rpm is probably the limit of the finger on a heavy bullpup trigger like this thing must have.
+			reloadTime = 0.24;
+		};
 	};
 
 	class srifle_GM6_F : GM6_base_F {
