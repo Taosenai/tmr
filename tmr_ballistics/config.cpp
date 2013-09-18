af0936326d0b88984d51feca31bb2d53fba016ba
Ryan <schultz.ryan@gmail.com>
2013-09-18 19:36:34 -0400

    ballistics: Values for all small arms

--- tmr_ballistics/config.cpp
+++ tmr_ballistics/config.cpp
@@ -43,13 +43,12 @@ class CfgAmmo {
 		// Fired from 14.5 in barrel
 		// Ref data calculated by Shooter Android app at alt: 0ft, hg: 29.92, temp 80 F, 0% humidity
 
-		// Pretend MX has 1:9 twist barrel
+		// Pretend MX has 1:7.5 twist barrel
 
-		typicalSpeed = 724; 
-		caliber = 1; // Default
+		typicalSpeed = 724;
+
+		// Determining experimentally with arma_bal.py (best match to subsonic using Shooter)
 		airFriction = -0.000915;
-
-		// This gives closely matching MV out to subsonic; gets bad after that due to Arma's simpler bullet model
 	};
 
 	class B_556x45_Ball : BulletBase {
@@ -57,8 +56,52 @@ class CfgAmmo {
 		// M855 load data
 		// Muzzle velocity based on 16" barrel (standard for F2000)
 		typicalSpeed = 911; // http://counterstrikefox.freeservers.com/mv.htm
-		airFriction = -0.001335; 
-		caliber = 0.8; // Default
+		airFriction = -0.001335;
+	};
+
+	class B_762x51_Ball: BulletBase {
+		typicalSpeed = 853; // Typical muzzle velocity of M240
+
+		// TODO: Correct airfriction for 7.62x51mm M80
+	};
+
+	class TMR_B_762x51_M118LR : B_762x51_Ball {
+		// http://www.snipercentral.com/m118.phtml
+		// 175gr Sierra HPBT MatchKing
+		// 1:12" rifling twist (probably, some civilian shooters like 1:10" for heavies)
+		// Mk14 Mod 1 barrel velocity of about 2600fps (18")
+		// http://www.militaryfactory.com/smallarms/detail.asp?smallarms_id=377
+
+		typicalspeed = 792;
+
+		// Determining experimentally with arma_bal.py (close match at 500m to ballistic calc data)
+		airfriction = -0.0008577;
+	};
+
+	// I will never understand WTF BI was thinking when they did this 9x21mm IMI bullshit.
+	class B_9x21_Ball;
+	class B_9x19_Ball : B_9x21_Ball {
+		// Cartridge: 9mm US M882 ball
+		// Bullet weight: 124 grains
+		// Bullet velocity: 1251fps +/- 25
+		// BC 0.160 123 gr round nose FMJ
+
+		typicalSpeed = 381;
+
+		// Determined with armabal.py (close ballistic match at 100 meters)
+		airfriction = -0.00213;
+	};
+
+	class B_45ACP_Ball: BulletBase {
+		// 230 gr (15 g) US Army Ball FMJ	830 ft/s (250 m/s)
+
+		// 230gr round nose FMJ
+		// Ballistic Coefficient (G1)	0.184
+		// Sectional Density	0.162
+		typicalSpeed = 250;
+
+		// Determined with armabal.py (close ballistic match at 100 meters)
+		airfriction = -0.0009;
 	};
 };
 
@@ -67,12 +110,10 @@ class CfgMagazines {
 	
 	class 30Rnd_65x39_caseless_mag : CA_Magazine {
 		initSpeed = 724; // initial MV for 14.5in barrel
-		descriptionshort = "Caliber: 6.5x39mm Caseless 120gr OTM<br />Rounds: 30<br />Used in: MX-1";
 	};
 
 	class 30Rnd_65x39_caseless_green : 30Rnd_65x39_caseless_mag {
 		initSpeed = 724; // initial MV for 14.5in barrel
-		descriptionshort = "Caliber: 6.5x38mm Caseless 120gr OTM<br />Rounds: 30<br />Used in: KT-2002";
 	};
 
 	class 100Rnd_65x39_caseless_mag : CA_Magazine {
@@ -86,6 +127,39 @@ class CfgMagazines {
 
 	class 30Rnd_556x45_Stanag : CA_Magazine {
 		initSpeed = 911; // Initial MV for a 16" barrel
+	};
+
+	class 20Rnd_762x51_Mag: CA_Magazine {
+		descriptionshort = "Caliber: 7.62x51mm M118LR<br />Rounds: 20<br />Type: M14";
+		initSpeed = 792; // 18" M14 EBR barrel
+		ammo = "TMR_B_762x51_M118LR"; // Use M118LR
+	};
+
+	class 150Rnd_762x51_Box : CA_Magazine {
+		ammo = "B_762x51_Ball";
+		initSpeed = 853; // Typical MV for M240
+	};
+
+	// Override the 9x21 mags to have 9x19 in them. Banish 9x21 to hell.
+	class 30Rnd_9x21_Mag : CA_Magazine {
+		ammo = "B_9x19_Ball";
+		initSpeed = 370; // Scorpion Evo 3A1 muzzle velocity according to CZ's manual
+		// Why is it so low, lower than a Glock? I'm not sure, but I have no choice but to believe them...!
+		// Maybe they are shooting some heavier bullet and not saying.
+	};
+
+	class 16Rnd_9x21_Mag : CA_Magazine {
+		ammo = "B_9x19_Ball";
+		// M9 initial MV. Close enough for horseshoes and handguns.
+		initSpeed = 381;
+	};
+
+	class 30Rnd_45ACP_Mag_SMG_01 : CA_Magazine {
+		initSpeed = 259; // From Vector manual (5.5" barrel)
+	};
+
+	class 9Rnd_45ACP_Mag : 30Rnd_45ACP_Mag_SMG_01 {
+		initSpeed = 250; // 1911
 	};
 };
 
@@ -114,7 +188,7 @@ class CfgWeapons {
 	class arifle_MX_Base_F : Rifle_Base_F {
 		class Single : Mode_SemiAuto {
 			dispersion = 0.000800; // radians. Equal to 2.75 MOA.
-			// Based on widely cited 2 MOA figure for 5.56 ACR.
+			// Based on widely cited 2 MOA figure for new 5.56 ACR.
 		};
 		class FullAuto : Mode_FullAuto {
 			dispersion = 0.00147; // radians. Equal to 5.1 MOA.
@@ -131,11 +205,31 @@ class CfgWeapons {
 		};
 	};
 
-	class LMG_Mk200_F : Rifle_Long_Base_F {
+	class arifle_katiba_Base_F : Rifle_Base_F {
 		class Single : Mode_SemiAuto {
+			dispersion = 0.000800; // radians. Equal to 2.75 MOA.
+			// Based on widely cited 2 MOA figure for new 5.56 ACR?
+			// Use your imagination for fictional weapons!
+		};
+		class FullAuto : Mode_FullAuto {
+			dispersion = 0.00147; // radians. Equal to 5.1 MOA.
+		};
+	};
+
+	class LMG_Mk200_F : Rifle_Long_Base_F {
+		class manual : Mode_FullAuto {
 			dispersion = 0.00175; // radians. Equal to 6 MOA.
 		};
+		class Single : manual {
+			dispersion = 0.00175; // radians. Equal to 6 MOA.
+		};
+	};
+
+	class LMG_Zafir_F : Rifle_Long_Base_F {
 		class manual : Mode_FullAuto {
+			dispersion = 0.00175; // radians. Equal to 6 MOA.
+		};
+		class Single : manual {
 			dispersion = 0.00175; // radians. Equal to 6 MOA.
 		};
 	};
@@ -159,4 +253,15 @@ class CfgWeapons {
 			dispersion = 0.00147; // radians. Equal to 5.1 MOA.
 		};
 	};
+
+	class SDAR_base_F : Rifle_Base_F {
+		class Single : Mode_SemiAuto {
+			dispersion = 0.0008727; // radians. Equal to 3 MOA, about the limit of mass-produced M855 plus
+			// some extra because Kel-Tec.
+		};
+		class FullAuto : Mode_FullAuto {
+			dispersion = 0.00147; // radians. Equal to 5.1 MOA.
+		};
+	};
+		
 };
