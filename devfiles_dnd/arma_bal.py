67c6f739c9848806543db54a51dcf2f25dceafa7
Ryan <schultz.ryan@gmail.com>
2013-09-18 19:36:46 -0400

    smallarms_recoil: Phase 2 recoil scheme, all weps

--- devfiles_dnd/arma_bal.py
+++ devfiles_dnd/arma_bal.py
@@ -1,7 +1,7 @@
 tt = 0
 
-vi = 911
-af = -0.001335
+vi = 381
+af = -0.0021;
 
 accel = af * vi * vi
 
@@ -13,7 +13,7 @@ drop = 0
 interval = 0.01
 i = 0
 
-while tt < 1.15:
+while tt < 0.5:
 	i = i + 1;
 	tt = tt + interval
 	dtrav = dtrav + (v * interval)
@@ -21,7 +21,7 @@ while tt < 1.15:
 		lastaccelcalc = dtrav
 		accel = af * v * v
 		drop = 0.5 * 9.80665 * tt * tt * 100
-		print(tt, dtrav, v, drop)
+		print(dtrav, v, drop)
 
 	#print(tt, dtrav, v)
 
--- tmr_smallarms_recoil/config.cpp
+++ tmr_smallarms_recoil/config.cpp
@@ -55,58 +55,68 @@ class Extended_FiredBIS_EventHandlers {
 //    time since fired   rear movement of gun   camera climb
 
 // Global muzzle climb increase over base. (For easy tuning.)
-#define TMR_MC 0.0009855
+#define TMR_MC 0.0089
 			// 0.0002755 // 3855
 
 // Global initial muzzle climb time from base. (For easy tuning.)
-#define TMR_IMCT -0.02
+#define TMR_IMCT -0.020
 
 // Global recovery sequence duration increase (For easy tuning.)
-#define TMR_RSD 0.021
+#define TMR_RSD 0.06031
              // 0.134
 
+#define TMR_RCT -0.0028
+
+ // 0.114 +tmr_RSD
 class CfgRecoils {
-	tmr_smallarms_recoil_single_mx[] = 				{0, 0, 0,		0.020+TMR_IMCT, 0.0115, 0.01622+TMR_MC,		0.134+TMR_RSD, 0, -0.0065,		0.143+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_mx[] = 				{0, 0, 0,		0.020+TMR_IMCT, 0.0115, 0.01622+TMR_MC,		0.134+TMR_RSD, 0, -0.0065,		0.143+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_single_prone_mx[] = 		{0, 0, 0,		0.020+TMR_IMCT, 0.0104, 0.01002+TMR_MC,		0.134+TMR_RSD, 0, -0.0044,		0.143+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_prone_mx[] = 			{0, 0, 0,		0.020+TMR_IMCT, 0.0104, 0.01007+TMR_MC,		0.134+TMR_RSD, 0, -0.0044,		0.143+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_mx[] = 				{0, 0.014, 0.024+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.081+TMR_RSD, 0, -0.0069+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_mx[] = 				{0, 0.014, 0.024+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.081+TMR_RSD, 0, -0.0072+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_prone_mx[] = 		{0, 0.014, 0.011+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.081+TMR_RSD, 0, -0.0036+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_prone_mx[] = 			{0, 0.014, 0.011+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.081+TMR_RSD, 0, -0.0038+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
 
-	tmr_smallarms_recoil_single_katiba[] = 			{0, 0, 0,		0.020+TMR_IMCT, 0.0115, 0.01552+TMR_MC,		0.134+TMR_RSD, 0, -0.0065,		0.143+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_katiba[] = 			{0, 0, 0,		0.020+TMR_IMCT, 0.0115, 0.01592+TMR_MC,		0.134+TMR_RSD, 0, -0.0065,		0.143+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_single_prone_katiba[] = 	{0, 0, 0,		0.020+TMR_IMCT, 0.0104, 0.00972+TMR_MC,		0.134+TMR_RSD, 0, -0.0044,		0.143+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_prone_katiba[] = 		{0, 0, 0,		0.020+TMR_IMCT, 0.0104, 0.00972+TMR_MC,		0.134+TMR_RSD, 0, -0.0044,		0.143+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_katiba[] = 			{0, 0.014, 0.022+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.078+TMR_RSD, 0, -0.0068+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_katiba[] = 			{0, 0.014, 0.022+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.078+TMR_RSD, 0, -0.0071+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_prone_katiba[] = 	{0, 0.014, 0.009+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.078+TMR_RSD, 0, -0.0035+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_prone_katiba[] = 		{0, 0.014, 0.009+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.078+TMR_RSD, 0, -0.0037+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
 
-	tmr_smallarms_recoil_single_mk20[] = 			{0, 0, 0,		0.020+TMR_IMCT, 0.0110, 0.01514+TMR_MC,		0.132+TMR_RSD, 0, -0.0065,		0.141+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_mk20[] = 				{0, 0, 0,		0.020+TMR_IMCT, 0.0110, 0.01519+TMR_MC,		0.132+TMR_RSD, 0, -0.0065,		0.141+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_single_prone_mk20[] = 		{0, 0, 0,		0.020+TMR_IMCT, 0.0102, 0.00900+TMR_MC,		0.132+TMR_RSD, 0, -0.0044,		0.141+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_prone_mk20[] = 		{0, 0, 0,		0.020+TMR_IMCT, 0.0102, 0.00905+TMR_MC,		0.132+TMR_RSD, 0, -0.0044,		0.141+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_mk20[] = 			{0, 0.013, 0.012+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.078+TMR_RSD, 0, -0.0033+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_mk20[] = 				{0, 0.013, 0.012+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.078+TMR_RSD, 0, -0.0034+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_prone_mk20[] = 		{0, 0.013, 0.002+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.078+TMR_RSD, 0, -0.0007+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_prone_mk20[] = 		{0, 0.013, 0.002+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.078+TMR_RSD, 0, -0.0008+TMR_RCT,		0.135+TMR_RSD, 0, 0};
 
-	tmr_smallarms_recoil_single_vermin[] = 			{0, 0, 0,		0.040+TMR_IMCT, 0.0120, 0.01300+TMR_MC,		0.129+TMR_RSD, 0, -0.0055,		0.138+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_vermin[] = 			{0, 0, 0,		0.040+TMR_IMCT, 0.0120, 0.01300+TMR_MC,		0.129+TMR_RSD, 0, -0.0055,		0.138+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_single_prone_vermin[] = 	{0, 0, 0,		0.040+TMR_IMCT, 0.0097, 0.00800+TMR_MC,		0.129+TMR_RSD, 0, -0.0034,		0.138+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_prone_vermin[] = 		{0, 0, 0,		0.040+TMR_IMCT, 0.0097, 0.00800+TMR_MC,		0.129+TMR_RSD, 0, -0.0034,		0.138+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_vermin[] = 			{0, 0.008, 0.011+TMR_MC,		0.047+TMR_RSD, 0, 0,		0.071+TMR_RSD, 0, -0.0031+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_vermin[] = 			{0, 0.008, 0.011+TMR_MC,		0.047+TMR_RSD, 0, 0,		0.071+TMR_RSD, 0, -0.0033+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_prone_vermin[] = 	{0, 0.008, 0.002+TMR_MC,		0.047+TMR_RSD, 0, 0,		0.071+TMR_RSD, 0, -0.0007+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_prone_vermin[] = 		{0, 0.008, 0.002+TMR_MC,		0.047+TMR_RSD, 0, 0,		0.071+TMR_RSD, 0, -0.0007+TMR_RCT,		0.135+TMR_RSD, 0, 0};
 
-	tmr_smallarms_recoil_single_scorpion[] = 		{0, 0, 0,		0.040+TMR_IMCT, 0.0109, 0.01460+TMR_MC,		0.129+TMR_RSD, 0, -0.0055,		0.138+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_scorpion[] = 			{0, 0, 0,		0.040+TMR_IMCT, 0.0109, 0.01460+TMR_MC,		0.129+TMR_RSD, 0, -0.0055,		0.138+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_single_prone_scorpion[] = 	{0, 0, 0,		0.040+TMR_IMCT, 0.0097, 0.00800+TMR_MC,		0.129+TMR_RSD, 0, -0.0034,		0.138+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_prone_scorpion[] = 	{0, 0, 0,		0.040+TMR_IMCT, 0.0097, 0.00800+TMR_MC,		0.129+TMR_RSD, 0, -0.0034,		0.138+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_scorpion[] = 		{0, 0.008, 0.012+TMR_MC,		0.046+TMR_RSD, 0, 0,		0.072+TMR_RSD, 0, -0.0030+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_scorpion[] = 			{0, 0.008, 0.013+TMR_MC,		0.046+TMR_RSD, 0, 0,		0.072+TMR_RSD, 0, -0.0033+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_prone_scorpion[] = 	{0, 0.008, 0.002+TMR_MC,		0.047+TMR_RSD, 0, 0,		0.071+TMR_RSD, 0, -0.0007+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_prone_scorpion[] = 	{0, 0.008, 0.002+TMR_MC,		0.047+TMR_RSD, 0, 0,		0.071+TMR_RSD, 0, -0.0007+TMR_RCT,		0.135+TMR_RSD, 0, 0};
 
-	tmr_smallarms_recoil_auto_mk200[] = 			{0, 0, 0,		0.043+TMR_IMCT, 0.0120, 0.01690+TMR_MC,		0.112+TMR_RSD, 0, -0.0065,		0.121+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_prone_mk200[] = 		{0, 0, 0,		0.043+TMR_IMCT, 0.0104, 0.00960+TMR_MC,		0.112+TMR_RSD, 0, -0.0044,		0.121+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_mk200[] = 			{0, 0.014, 0.021+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.079+TMR_RSD, 0, -0.0063+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_mk200[] = 			{0, 0.014, 0.021+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.079+TMR_RSD, 0, -0.0143+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_prone_mk200[] = 	{0, 0.014, 0.002+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.077+TMR_RSD, 0, -0.0037+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_prone_mk200[] = 		{0, 0.014, 0.002+TMR_MC,		0.048+TMR_RSD, 0, 0,		0.077+TMR_RSD, 0, -0.0041+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
 
-	tmr_smallarms_recoil_auto_zafir[] = 			{0, 0, 0,		0.040+TMR_IMCT, 0.0140, 0.02570+TMR_MC,		0.122+TMR_RSD, 0, -0.0065,		0.131+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_prone_zafir[] = 		{0, 0, 0,		0.040+TMR_IMCT, 0.0120, 0.01230+TMR_MC,		0.122+TMR_RSD, 0, -0.0044,		0.131+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_zafir[] = 			{0, 0.015, 0.026+TMR_MC,		0.046+TMR_RSD, 0, 0,		0.082+TMR_RSD, 0, -0.0072+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_zafir[] = 			{0, 0.015, 0.026+TMR_MC,		0.046+TMR_RSD, 0, 0,		0.082+TMR_RSD, 0, -0.0147+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_prone_zafir[] = 	{0, 0.015, 0.004+TMR_MC,		0.046+TMR_RSD, 0, 0,		0.080+TMR_RSD, 0, -0.0017+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_prone_zafir[] = 		{0, 0.015, 0.004+TMR_MC,		0.046+TMR_RSD, 0, 0,		0.080+TMR_RSD, 0, -0.0070+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
 
-	tmr_smallarms_recoil_single_ebr[] = 			{0, 0, 0,		0.040+TMR_IMCT, 0.0140, 0.02600+TMR_MC,		0.132+TMR_RSD, 0, -0.0065,		0.138+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_ebr[] = 				{0, 0, 0,		0.040+TMR_IMCT, 0.0140, 0.02750+TMR_MC,		0.132+TMR_RSD, 0, -0.0065,		0.138+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_single_prone_ebr[] = 		{0, 0, 0,		0.040+TMR_IMCT, 0.0120, 0.01150+TMR_MC,		0.132+TMR_RSD, 0, -0.0044,		0.138+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_auto_prone_ebr[] = 		{0, 0, 0,		0.040+TMR_IMCT, 0.0120, 0.01350+TMR_MC,		0.132+TMR_RSD, 0, -0.0044,		0.138+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_ebr[] = 			{0, 0.014, 0.028+TMR_MC,		0.045+TMR_RSD, 0, 0,		0.084+TMR_RSD, 0, -0.0074+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_ebr[] = 				{0, 0.014, 0.028+TMR_MC,		0.045+TMR_RSD, 0, 0,		0.084+TMR_RSD, 0, -0.0077+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_prone_ebr[] = 		{0, 0.014, 0.013+TMR_MC,		0.045+TMR_RSD, 0, 0,		0.084+TMR_RSD, 0, -0.0038+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_auto_prone_ebr[] = 		{0, 0.014, 0.013+TMR_MC,		0.045+TMR_RSD, 0, 0,		0.084+TMR_RSD, 0, -0.0041+TMR_RCT, 		0.135+TMR_RSD, 0, 0};
 
-	tmr_smallarms_recoil_single_lrr[] = 			{0, 0, 0,		0.010+TMR_IMCT, 0.0350, 0.10200+TMR_MC,										0.430+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_single_prone_lrr[] = 		{0, 0, 0,		0.010+TMR_IMCT, 0.0310, 0.03300+TMR_MC,		0.360+TMR_RSD, 0, -0.0100,		0.430+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_lrr[] = 			{0, 0.030, 0.114+TMR_MC,		0.043+TMR_RSD, 0, 0,		0.250+TMR_RSD, 0, -0.0130+TMR_RCT,		0.430+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_prone_lrr[] = 		{0, 0.030, 0.074+TMR_MC,		0.043+TMR_RSD, 0, 0,		0.250+TMR_RSD, 0, -0.0100+TMR_RCT,		0.430+TMR_RSD, 0, 0};
 
-	tmr_smallarms_recoil_single_gm6[] = 			{0, 0, 0,		0.010+TMR_IMCT, 0.0350, 0.10200+TMR_MC,										0.430+TMR_RSD, 0, 0};
-	tmr_smallarms_recoil_single_prone_gm6[] = 		{0, 0, 0,		0.010+TMR_IMCT, 0.0310, 0.03300+TMR_MC,		0.360+TMR_RSD, 0, 0,			0.430+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_gm6[] = 			{0, 0.030, 0.114+TMR_MC,		0.043+TMR_RSD, 0, 0,		0.250+TMR_RSD, 0, -0.0130+TMR_RCT,		0.430+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_prone_gm6[] = 		{0, 0.030, 0.074+TMR_MC,		0.043+TMR_RSD, 0, 0,		0.250+TMR_RSD, 0, -0.0100+TMR_RCT,		0.430+TMR_RSD, 0, 0};
+
+	tmr_smallarms_recoil_single_pistol9mm[] = 		{0, 0.010, 0.029+TMR_MC,		0.046+TMR_RSD, 0, 0,		0.090+TMR_RSD, 0, -0.0075+TMR_RCT,		0.135+TMR_RSD, 0, 0};
+	tmr_smallarms_recoil_single_pistol45[] = 		{0, 0.010, 0.033+TMR_MC,		0.047+TMR_RSD, 0, 0,		0.085+TMR_RSD, 0, -0.0079+TMR_RCT,		0.135+TMR_RSD, 0, 0};
 };
 
 class CfgCameraShake {
@@ -214,6 +224,19 @@ class CfgWeapons {
 		};
 	};
 
+	class arifle_MX_SW_F : arifle_MX_Base_F {
+		tmr_smallarms_recoil_shakeMultiplier = 0.95;
+		class Single : Mode_SemiAuto {
+			recoil = "tmr_smallarms_recoil_single_mx";
+			recoilProne = "tmr_smallarms_recoil_single_prone_mx";
+		};
+		
+		class manual : FullAuto {
+			recoil = "tmr_smallarms_recoil_auto_mx";
+			recoilProne = "tmr_smallarms_recoil_auto_prone_mx";
+		};
+	};
+
 	class arifle_katiba_Base_F : Rifle_Base_F {	
 		tmr_smallarms_recoil_shakeMultiplier = 1;
 		class Single : Mode_SemiAuto {
@@ -228,6 +251,19 @@ class CfgWeapons {
 	};
 
 	class mk20_base_F : Rifle_Base_F {
+		tmr_smallarms_recoil_shakeMultiplier = 0.9;
+		class Single : Mode_SemiAuto {
+			recoil = "tmr_smallarms_recoil_single_mk20";
+			recoilProne = "tmr_smallarms_recoil_single_prone_mk20";
+		};
+		
+		class FullAuto : Mode_FullAuto {
+			recoil = "tmr_smallarms_recoil_auto_mk20";
+			recoilProne = "tmr_smallarms_recoil_auto_prone_mk20";
+		};
+	};
+
+	class arifle_Mk20C_F : mk20_base_F {
 		tmr_smallarms_recoil_shakeMultiplier = 0.9;
 		class Single : Mode_SemiAuto {
 			recoil = "tmr_smallarms_recoil_single_mk20";
@@ -268,7 +304,7 @@ class CfgWeapons {
 
 	////////////////////////////////////////////////
 
-	class SMG_01_Base: Rifle_Base_F {
+	class SMG_01_Base : Rifle_Base_F {
 		tmr_smallarms_recoil_shakeMultiplier = 0.9;
 		class Single : Mode_SemiAuto {
 			recoil = "tmr_smallarms_recoil_single_vermin";
@@ -286,7 +322,7 @@ class CfgWeapons {
 		};
 	};
 
-	class SMG_02_Base: Rifle_Base_F {
+	class SMG_02_Base_F : Rifle_Base_F {
 		tmr_smallarms_recoil_shakeMultiplier = 0.9;
 		class Single : Mode_SemiAuto {
 			recoil = "tmr_smallarms_recoil_single_scorpion";
@@ -304,27 +340,40 @@ class CfgWeapons {
 		};
 	};
 
+	class pdw2000_base_F: Rifle_Base_F {
+		tmr_smallarms_recoil_shakeMultiplier = 0.9;
+		class Single : Mode_SemiAuto {
+			recoil = "tmr_smallarms_recoil_single_scorpion";
+			recoilProne = "tmr_smallarms_recoil_single_prone_scorpion";
+		};
+		
+		class FullAuto : Mode_FullAuto {
+			recoil = "tmr_smallarms_recoil_auto_scorpion";
+			recoilProne = "tmr_smallarms_recoil_auto_prone_scorpion";
+		};
+	};
+
 	////////////////////////////////////////////////
 
 	class Rifle_Long_Base_F;
 
 	class LMG_Mk200_F : Rifle_Long_Base_F {
-		tmr_smallarms_recoil_shakeMultiplier = 1.2;
-		class Single : Mode_SemiAuto {
+		tmr_smallarms_recoil_shakeMultiplier = 1.1;
+		class manual : Mode_FullAuto {
 			recoil = "tmr_smallarms_recoil_auto_mk200";
 			recoilprone = "tmr_smallarms_recoil_auto_prone_mk200";
 		};
-		class manual: Mode_FullAuto {
-			recoil = "tmr_smallarms_recoil_auto_mk200";
-			recoilprone = "tmr_smallarms_recoil_auto_prone_mk200";
+		class Single : manual {
+			recoil = "tmr_smallarms_recoil_single_mk200";
+			recoilprone = "tmr_smallarms_recoil_single_prone_mk200";
 		};
 	};
 
 	class LMG_Zafir_F: Rifle_Long_Base_F {
-		tmr_smallarms_recoil_shakeMultiplier = 1.6;
+		tmr_smallarms_recoil_shakeMultiplier = 1.3;
 		class Single: Mode_SemiAuto {
-			recoil = "tmr_smallarms_recoil_auto_zafir";
-			recoilprone = "tmr_smallarms_recoil_auto_prone_zafir";
+			recoil = "tmr_smallarms_recoil_single_zafir";
+			recoilprone = "tmr_smallarms_recoil_single_prone_zafir";
 		};
 		class FullAuto: Mode_FullAuto {
 			recoil = "tmr_smallarms_recoil_auto_zafir";
@@ -335,7 +384,7 @@ class CfgWeapons {
 	////////////////////////////////////////////////
 
 	class EBR_base_F : Rifle_Long_Base_F {
-		tmr_smallarms_recoil_shakeMultiplier = 1.6;
+		tmr_smallarms_recoil_shakeMultiplier = 1.3;
 		class Single : Mode_SemiAuto {
 			recoil = "tmr_smallarms_recoil_single_ebr";
 			recoilProne = "tmr_smallarms_recoil_single_prone_ebr";
@@ -348,7 +397,7 @@ class CfgWeapons {
 	};
 
 	class LRR_base_F : Rifle_Long_Base_F {
-		tmr_smallarms_recoil_shakeMultiplier = 3.7;
+		tmr_smallarms_recoil_shakeMultiplier = 2.3;
 		class Single : Mode_SemiAuto {
 			recoil = "tmr_smallarms_recoil_single_lrr";
 			recoilProne = "tmr_smallarms_recoil_single_prone_lrr";
@@ -356,10 +405,28 @@ class CfgWeapons {
 	};
 
 	class GM6_base_F : Rifle_Long_Base_F {
-		tmr_smallarms_recoil_shakeMultiplier = 3.6;
+		tmr_smallarms_recoil_shakeMultiplier = 2.3;
 		class Single : Mode_SemiAuto {
 			recoil = "tmr_smallarms_recoil_single_gm6";
 			recoilProne = "tmr_smallarms_recoil_single_prone_gm6";
 		};
+	};
+
+	/////////////////////////////////////////////////
+
+	class Pistol_Base_F;
+	class hgun_P07_F : Pistol_Base_F {
+		recoil = "tmr_smallarms_recoil_single_pistol9mm";
+		recoilProne = "tmr_smallarms_recoil_single_pistol9mm";
+	};
+
+	class hgun_Rook40_F : Pistol_Base_F {
+		recoil = "tmr_smallarms_recoil_single_pistol9mm";
+		recoilProne = "tmr_smallarms_recoil_single_pistol9mm";
+	};
+
+	class hgun_ACPC2_F : Pistol_Base_F {
+		recoil = "tmr_smallarms_recoil_single_pistol45";
+		recoilProne = "tmr_smallarms_recoil_single_pistol45";
 	};
 };
